064026d81d54b4de6a066c517ef4b4f81e1b5219
Lele <mcknightlejana@gmail.com>
2026-06-13 00:54:50 -0400

    added more variations of "yes" for the user to choose from, and added a thank you message at the end of the program.

--- Example_04_19_26.py
+++ Example_04_19_26.py
@@ -7,7 +7,7 @@ cakes = "raspberry lemon ice box", "coconut lime", "carmel cake", 'strawberry sh
 print(str(cakes))
 order_two = input("What flavor of cake would you like to order? ").lower()
 if order_two in cakes:
-        if input("Would you like to add fruit to your cake for an additional 50 cents? ").lower() == "yes" or "sure":
+        if input("Would you like to add fruit to your cake for an additional 50 cents? ").lower() in ["yes", "sure", "Y", "yeah", "yep"]:
             print(fruit)
             new_fruit = input("What kind of fruit would you like to add to your cake? ").lower()
             print("Great! We will add " + new_fruit + " to your " + order_two + " cake for an additional 50 cents.")
@@ -16,4 +16,4 @@ if order_two in cakes:
 else:
     print("Sorry, We do not have that flavor in stock. Please choose from the following options: " + str(cakes))
 
-print("Thank you for ordering at Lele\'s Sweets & Treats!")
+print("Thank you for ordering at Lele\'s Sweets & Treats! Enjoy :)")
